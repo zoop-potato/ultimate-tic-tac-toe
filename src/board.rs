29ff4a490e1b64405793d6250ea8c8db9c421dbd
zoop-potato <joshbutler2084@gmail.com>
2023-10-10 23:54:41 -0400

    Initial Commit

---
+++ src/board.rs
@@ -0,0 +1,133 @@
+struct UTTTBoard {
+    state: Option<FinishState>,
+    board: [TTTBoard; 9],
+}
+
+struct TTTBoard {
+    state: Option<FinishState>,
+    board: [Option<Player>; 9],
+}
+enum FinishState {
+    Win(Player),
+    Draw,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum Player {
+    X,
+    O,
+}
+
+enum BoardPosition {
+    TopLeft,
+    TopMiddle,
+    TopRight,
+    MiddleLeft,
+    Center,
+    MiddleRight,
+    BottomLeft,
+    BottomMiddle,
+    BottomRight,
+}
+
+impl BoardPosition {
+    fn to_index(self) -> usize {
+        match self {
+            BoardPosition::TopLeft => 0,
+            BoardPosition::TopMiddle => 1,
+            BoardPosition::TopRight => 2,
+            BoardPosition::MiddleLeft => 3,
+            BoardPosition::Center => 4,
+            BoardPosition::MiddleRight => 5,
+            BoardPosition::BottomLeft => 6,
+            BoardPosition::BottomMiddle => 7,
+            BoardPosition::BottomRight => 8,
+        }
+    }
+}
+
+static WINNING_LINES: [[usize; 3]; 8] = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+
+impl TTTBoard {
+    fn play(mut self, player: Player, position: BoardPosition) -> Option<Option<FinishState>> {
+        if self.state.is_some() {
+            // Board is already Complete
+            return None;
+        }
+
+        let position_state = self.board.get_mut(position.to_index())?;
+
+        match position_state {
+            // This postion has already been played in
+            Some(_) => return None,
+            // Succsesfully play at position
+            None => *position_state = Some(player),
+        }
+
+        let finish = self.check_for_finish();
+        match finish {
+            // The board is full or has been won
+            Some(x) => return Some(Some(x)),
+            None => {}
+        }
+
+        // play succeeded and play is still possible
+        return Some(None);
+    }
+
+    fn check_for_finish(self) -> Option<FinishState> {
+        // map every index in WINNING_LINES to its value
+        let mapped_lines = WINNING_LINES
+            .iter()
+            .map(|line|
+                // Turn an array of indexs into the value of their board position
+                line.iter()
+                    .map(|index| self.board[*index])
+                    .collect::<Vec<Option<Player>>>())
+            .collect::<Vec<Vec<Option<Player>>>>();
+
+        // Check if X won
+        let x_won = mapped_lines
+            .iter()
+            .map(|line| line.iter().all(|position| *position == Some(Player::X)))
+            .any(|line_win| line_win == true);
+        if x_won { return Some(FinishState::Win(Player::X));}
+
+        // Check if O won
+        let o_won = mapped_lines
+            .iter()
+            .map(|line| line.iter().all(|position| *position == Some(Player::O)))
+            .any(|line_win| line_win == true);
+        if o_won { return Some(FinishState::Win(Player::O));}
+
+        // Check for draw
+        let full = self
+            .board
+            .iter()
+            .filter(|position_state| position_state.is_none())
+            .collect::<Vec<&Option<Player>>>()
+            .is_empty();
+        match full {
+            // the board is full, therefore draw
+            true => return Some(FinishState::Draw),
+            // no player has won and spaces are open
+            false => return None,
+        }
+    }
+
+    fn new() -> Self {
+        Self {
+            state: None,
+            board: [None; 9],
+        }
+    }
+}
