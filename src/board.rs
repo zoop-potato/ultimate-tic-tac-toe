44a3cf888cbaaeec22ffadd62a8b920787b702d4
zoop-potato <joshbutler2084@gmail.com>
2023-10-13 22:20:43 -0400

    Create Board trait

---
+++ src/board.rs
@@ -0,0 +1,12 @@
+pub trait Board {
+    type Player;
+    type PlayResult;
+    type Move;
+    type Finish;
+
+    fn play(&mut self, player: Self::Player, choice: Self::Move) -> Self::PlayResult;
+
+    fn check_for_finish(&self) -> Self::Finish;
+
+    fn new_board() -> Self;
+}
